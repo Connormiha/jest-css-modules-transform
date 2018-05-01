7b1572d41e1eebdc936c0ce851dde5c958f5ed45
Mihail Bodrov <connormiha1@gmail.com>
2018-05-02 01:32:52 +0300

    Add test for nested selectors :white_check_mark:

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -3,6 +3,7 @@ const stylusFile = require('./source/style.styl').default;
 const sassFile = require('./source/style.sass').default;
 const scssFile = require('./source/style.scss').default;
 const lessFile = require('./source/style.less').default;
+const cssNestedFile = require('./source/style-nested.css').default;
 const cssFileExpect = require('./source/expected');
 
 describe('jest-css-modules', () => {
@@ -24,5 +25,14 @@ describe('jest-css-modules', () => {
 
     it('should import .less', () => {
         expect(lessFile).toEqual(cssFileExpect);
+    });
+
+    it('should import .css with nested', () => {
+        expect(cssNestedFile).toEqual({
+            foo: 'foo',
+            foo__bar: 'foo__bar',
+            foo__zoo: 'foo__zoo',
+            boo: 'boo',
+        });
     });
 });
