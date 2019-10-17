4cb62289f68a8dd46e6c8333c01a09c6b1a144cd
Mihail Bodrov <bodrovmiha@yandex.ru>
2019-10-17 03:05:55 +0300

    Use strongly unix line-break :art:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,110 +1,111 @@
 module.exports = {
-    "extends": [
-      "eslint:all",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended"
+    extends: [
+      'eslint:all',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended'
     ],
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-      "jest",
-      "@typescript-eslint"
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      'jest',
+      '@typescript-eslint'
     ],
-    "overrides": [
+    overrides: [
       {
-        "files": [
-          "__tests__/**/*.spec.js"
+        files: [
+          '__tests__/**/*.spec.js'
         ],
-        "env": {
-          "jest": true
+        env: {
+          jest: true
         }
       },
       {
-        "files": [
-          "src/**/*.ts"
+        files: [
+          'src/**/*.ts'
         ]
       }
     ],
-    "env": {
-      "es6": true,
-      "node": true
+    env: {
+      es6: true,
+      node: true
     },
-    "parserOptions": {
-      "sourceType": "module"
+    parserOptions: {
+      sourceType: 'module'
     },
-    "rules": {
+    rules: {
       '@typescript-eslint/interface-name-prefix': ['error', {
         prefixWithI: 'always',
       }],
-      "quotes": [
-        "error",
-        "single"
+      quotes: [
+        'error',
+        'single'
       ],
-      "space-before-function-paren": [
-        "error",
-        "never"
+      'space-before-function-paren': [
+        'error',
+        'never'
       ],
-      "padded-blocks": [
-        "error",
-        "never"
+      'padded-blocks': [
+        'error',
+        'never'
       ],
-      "quote-props": [
-        "error",
-        "as-needed"
+      'quote-props': [
+        'error',
+        'as-needed'
       ],
-      "sort-imports": "off",
-      "id-length": "off",
-      "sort-keys": "off",
-      "camelcase": "off",
-      "no-magic-numbers": "off",
-      "function-paren-newline": "off",
-      "function-call-argument-newline": "off",
-      "no-extra-parens": "off",
-      "default-case": "off",
-      "no-plusplus": "off",
-      "strict": "off",
-      "capitalized-comments": "off",
-      "object-property-newline": "off",
-      "dot-location": "off",
-      "multiline-ternary": "off",
-      "max-statements": "off",
-      "no-ternary": "off",
-      "init-declarations": "off",
-      "array-element-newline": "off",
-      "valid-jsdoc": "off",
-      "no-sync": "off",
-      "func-style": "off",
-      "no-process-env": "off",
-      "no-param-reassign": "off",
-      "max-lines-per-function": "off",
-      "global-require": "off",
-      "require-jsdoc": "off",
-      "prefer-named-capture-group": "off",
-      "no-prototype-builtins": "off",
-      "no-continue": "off",
-      "no-undefined": "off",
-      "no-underscore-dangle": "off",
-      "require-unicode-regexp": "off",
-      "max-params": [
-        "error",
+      'sort-imports': 'off',
+      'id-length': 'off',
+      'sort-keys': 'off',
+      camelcase: 'off',
+      'no-magic-numbers': 'off',
+      'function-paren-newline': 'off',
+      'function-call-argument-newline': 'off',
+      'no-extra-parens': 'off',
+      'default-case': 'off',
+      'no-plusplus': 'off',
+      strict: 'off',
+      'capitalized-comments': 'off',
+      'object-property-newline': 'off',
+      'dot-location': 'off',
+      'multiline-ternary': 'off',
+      'max-statements': 'off',
+      'no-ternary': 'off',
+      'init-declarations': 'off',
+      'array-element-newline': 'off',
+      'valid-jsdoc': 'off',
+      'no-sync': 'off',
+      'linebreak-style': ['error', 'unix'],
+      'func-style': 'off',
+      'no-process-env': 'off',
+      'no-param-reassign': 'off',
+      'max-lines-per-function': 'off',
+      'global-require': 'off',
+      'require-jsdoc': 'off',
+      'prefer-named-capture-group': 'off',
+      'no-prototype-builtins': 'off',
+      'no-continue': 'off',
+      'no-undefined': 'off',
+      'no-underscore-dangle': 'off',
+      'require-unicode-regexp': 'off',
+      'max-params': [
+        'error',
         5
       ],
-      "one-var": [
-        "error",
-        "never"
+      'one-var': [
+        'error',
+        'never'
       ],
-      "indent": [
-        "error",
+      indent: [
+        'error',
         4,
         {
-          "SwitchCase": 1
+          SwitchCase: 1
         }
       ],
-      "comma-dangle": [
-        "error",
-        "always-multiline"
+      'comma-dangle': [
+        'error',
+        'always-multiline'
       ],
-      "max-len": [
-        "error",
+      'max-len': [
+        'error',
         140
       ]
     }
