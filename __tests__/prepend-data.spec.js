11f3a26656493270703e0d5536bdcbf7dc3412cb
Mikhail Bodrov <bodrovmiha@yandex.ru>
2020-12-16 02:16:10 +0200

    Add test for sass importer :white_check_mark:

--- __tests__/prepend-data.spec.js
+++ __tests__/prepend-data.spec.js
@@ -4,13 +4,12 @@ const path = require('path');
 describe('jest-css-modules', () => {
     const configPath = path.resolve(__dirname, '..', 'jest-css-modules-transform-config.js');
     const prependFilePath = path.resolve(__dirname, '..', 'prepended-file.styl');
+    const mainSassFilePath = path.resolve(__dirname, '..', 'main-sass-file.scss');
+    const importSassFileBadPath = path.resolve(__dirname, '..', 'import-sass-file-bad.scss');
+    const importSassFilePath = path.resolve(__dirname, '..', 'import-sass-file.scss');
 
     describe('prependData', () => {
         beforeAll(() => {
-            const data = {
-                prepend: prependFilePath,
-            };
-
             fs.writeFileSync(
                 prependFilePath,
                 `.prepended-class {
@@ -19,14 +18,49 @@ describe('jest-css-modules', () => {
             );
 
             fs.writeFileSync(
+                mainSassFilePath,
+                `
+                    @import '${importSassFileBadPath}';
+                    .class { color: $specColor; }
+                `,
+            );
+
+            fs.writeFileSync(
+                importSassFilePath,
+                '.class2 { color: $specColor; }',
+            );
+
+            fs.writeFileSync(
+                importSassFilePath,
+                '$specColor: red;',
+            );
+
+            fs.writeFileSync(
                 configPath,
-                `module.exports = ${JSON.stringify(data)}`
+                `module.exports = {
+                    prepend: '${prependFilePath}',
+                    sassConfig: {
+                        importer: [
+                            (url) => {
+                                if (!url.includes('${importSassFileBadPath}')) {
+                                    return null;
+                                }
+    
+                                return {
+                                    file: '${importSassFilePath}',
+                                };
+                            },
+                        ],
+                    },
+                }`
             );
         });
 
         afterAll(() => {
             fs.unlinkSync(configPath);
             fs.unlinkSync(prependFilePath);
+            fs.unlinkSync(mainSassFilePath);
+            fs.unlinkSync(importSassFilePath);
         });
 
         it('should have correct object params stylus', () => {
@@ -59,6 +93,11 @@ describe('jest-css-modules', () => {
             expect(cssFile).toBeInstanceOf(Object);
             expect(Object.keys(cssFile)).toMatchSnapshot();
             expect(cssFile['prepended-class']).toBe('prepended-class');
+        });
+
+        it('should have correct object params scss with importer', () => {
+            const cssFile = require(mainSassFilePath);
+            expect(typeof cssFile).toBe('object');
         });
     });
 });
